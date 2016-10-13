af0b80dc16e31b0888228dc93d096036feb3e077
Sölvi Páll Ásgeirsson <solvip@gmail.com>
2016-10-13 16:45:02 +0000

    Add a doc comment for add()

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,6 +1,13 @@
 package is.ru.stringcalculator;
 
 class Calculator {
+
+    /**
+     * Takes a string of comma-seperated integer values and returns their sum.
+     * The empty string is equivalent to 0(what a crazy API)
+     * @param input Comma-seperated integer values
+     * @return The sum of the input
+     */
     public static int add(String input) {
         if(input == "") {
             return 0;
