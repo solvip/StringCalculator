cb363cbbe82a8744cfcea99e55c4d3cc71b766d0
Sölvi Páll Ásgeirsson <solvip@gmail.com>
2016-10-13 16:42:26 +0000

    Implement the add method + tests

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,5 +1,16 @@
 package is.ru.stringcalculator;
 
 class Calculator {
+    public static int add(String input) {
+        if(input == "") {
+            return 0;
+        }
 
+        int sum = 0;        
+        for(String integer : input.split(",")) {
+            sum += Integer.parseInt(integer);
+        }
+
+        return sum;
+    }
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -1,6 +1,19 @@
 package is.ru.stringcalculator;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CalculatorTest {
-    
+
+    @Test
+    public void testEmptyString() {
+        Assert.assertEquals(0, Calculator.add(""));
+    }
+
+    @Test
+    public void testSimpleSums() {
+        Assert.assertEquals(1, Calculator.add("1"));
+        Assert.assertEquals(3, Calculator.add("1,2"));
+    }
 }
 
